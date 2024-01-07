1516b25fe28591479af7484eafa91f36131a640a
rostislavdmrv <127777371+rostislavdmrv@users.noreply.github.com>
2024-01-07 21:03:42 +0200

    Create index.php

---
+++ UploadFile/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<!--
+Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edit this template
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        $file = fopen("data.txt", "w");
+        $text = "Дисциплина Web приложения се изучава през втория семестър на втори курс. Тази дисциплина се изучава от студенти, които са специалност СИТ.";
+        fwrite($file, $text);
+        fclose($file);
+
+        $file = fopen("data.txt", "a");
+        $text = "\nСтудентите редовно си пишат домашните.";
+        fwrite($file, $text);
+        fclose($file);
+        
+        $file = fopen("data.txt", "r");
+        $content = fread($file, filesize("data.txt"));
+        fclose($file);
+        echo $content . "<br>";
+        echo "Размер на файла: " . filesize("data.txt") . " байта.";
+        ?>
+    </body>
+</html>
