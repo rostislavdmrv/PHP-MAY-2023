0ac3c428d89b777b61bbf3af38a4eba94a84bb6d
rostislavdmrv <127777371+rostislavdmrv@users.noreply.github.com>
2024-01-12 21:20:12 +0200

    Create zad2.php

---
+++ Functions/zad2.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<!--
+Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHPWebPage.php to edit this template
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <form method="post" action="#">
+            <p>Въведете положителни стойности:</p>
+            <br>
+	    a:
+            <input type="number"  name="a">
+            <br>
+	    b:
+            <input type="number"  name="b">
+            <br>
+            c:
+            <input type="number"  name="c">
+            
+            <br>
+            <br>
+            <input type="submit"  name="submit"  value="Провери">
+	</form>
+        <?php
+        
+        
+        ?>
+    </body>
+</html>
