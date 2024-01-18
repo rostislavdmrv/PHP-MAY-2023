6be51d3a451d3e177dd1fdb01cdcd00a060a2321
rostislavdmrv <127777371+rostislavdmrv@users.noreply.github.com>
2024-01-18 17:16:51 +0200

    Create zadacha2.php

---
+++ MagicFunctions/zadacha2.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<!--
+Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHPWebPage.php to edit this template
+-->
+<?php session_start();?>
+<html>
+    <head>
+        <meta  http-equiv="content-type" content="text/html;charset="UTF-8">
+        <title>Web Browser </title>
+    </head>
+    <body>
+        <?php
+        
+        ?>
+    </body>
+</html>
