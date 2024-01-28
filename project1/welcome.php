d839cf8f1eeccb7eda0deca869675cd0847a5b06
rostislavdmrv <127777371+rostislavdmrv@users.noreply.github.com>
2024-01-28 23:30:03 +0200

    Create welcome.php

---
+++ project1/welcome.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<!--
+Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edit this template
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+       
+        <?php
+        print_r($_COOKIE);
+        // put your code here
+        ?> 
+        <a href="welcome.php"> Go to last page,delete cookie</a>
+    </body>
+</html>
