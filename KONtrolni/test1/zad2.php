d5a1dd1ed496587a26fabd69101003167d816495
rostislavdmrv <127777371+rostislavdmrv@users.noreply.github.com>
2024-03-03 23:46:35 +0200

    Create zad2.php

---
+++ KONtrolni/test1/zad2.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<!--
+Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHPWebPage.php to edit this template
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        switch ($i) {
+    case 0:
+        echo "i equals 0";
+        break;
+    case 1:
+        echo "i equals 1";
+        break;
+    case 2:
+        echo "i equals 2";
+        break;
+}
+        /*function findMinMaxAndEvenAverage($numbers) {
+            $min = $numbers[0]; 
+            $max = $numbers[0]; 
+            $sum = 0; 
+            $count = 0; 
+        
+        foreach($numbers as $num) {
+            if ($num < $min) {
+            $min = $num;
+        }
+        
+        if ($num > $max) {
+            $max = $num;
+        }
+        
+        if ($num % 2 == 0) {
+            $sum += $num; 
+            $count++; }
+            
+        }
+
+        $average = $sum / $count;
+
+        echo "Минимална стойност: " . $min . "<br>";
+        echo "Максимална стойност: " . $max . "<br>";
+        echo "Средноаритметична стойност : " . $average . "<br>";}
+
+       $numbers = array(56, 5, -22, 1, -3, 34, 14, 6, 17, 10);
+
+       findMinMaxAndEvenAverage($numbers);*/
+        
+        ?>
+    </body>
+</html>
