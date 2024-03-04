19add83771d24e3b8ae316fdefc578ead436eaf7
rostislavdmrv <127777371+rostislavdmrv@users.noreply.github.com>
2024-03-04 19:08:05 +0200

    Create index.php

---
+++ FirstDB/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<!--
+Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edit this template
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <form method="POST" action="">
+        <label for="title">Title:</label>
+        <input type="text" id="title" name="title"><br><br>
+        <label for="author">Author:</label>
+        <input type="text" id="author" name="author"><br><br>
+        <label for="publisher">Publisher:</label>
+        <input type="text" id="publisher" name="publisher"><br><br>
+        <label for="year">Year:</label>
+        <input type="text" id="year" name="year"><br><br>
+        <input type="submit" name="submit" value="Submit">
+    </form>
+        <?php
+        
+        ?>
+    </body>
+</html>
